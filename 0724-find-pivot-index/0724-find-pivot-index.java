5348edd542cce221924973744686f17d563982d0
Bhagya-Shah <91955020+Bhagya-Shah@users.noreply.github.com>
2023-01-20 13:24:32 +0530

    Time: 145 ms (19.12%), Space: 43.4 MB (54.95%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,19 @@
 class Solution {
-    public boolean ispivot(int[] nums,int index){
-        int ls=0,rs=0;
+    public boolean ispivot(int[] nums,int index,int sum){
+        int ls=0;
         for(int i=0;i<index;i++){
             ls+=nums[i];
         }
-        for(int i=nums.length-1;i>index;i--){
-            rs+=nums[i];
-        }
-        if(ls==rs){
+        if(ls==sum-ls-nums[index]){
             return true;
         }
         return false;
     }
     public int pivotIndex(int[] nums) {
+        int sum=0;
+        for(int i:nums) sum+=i;
         for(int i=0;i<nums.length;i++){
-            if(ispivot(nums,i)){
+            if(ispivot(nums,i,sum)){
                 return i;
             }
         }
